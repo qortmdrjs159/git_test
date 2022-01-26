c3354180f2d88d02fe157773ceb818edc47a3cab
Sungjinn Chung <s.chung@lexo.co.kr>
2022-01-26 17:25:45 +0900

    isnt

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-git diff > test.patch
+git diff git_test.py > test.patch
 
 if test ! -s "test.patch"
 then
@@ -7,7 +7,7 @@ else
     rm -rf git_test
     git clone  https://github.com/qortmdrjs159/git_test.git
     qortmdrjs159
-    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 ;
+    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 ;
     echo is old version, update..
 fi
 
