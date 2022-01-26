bc7da31b37c8ae599e975a7f0e2e5d3267547c25
Sungjinn Chung <s.chung@lexo.co.kr>
2022-01-26 15:17:02 +0900

    auto update

---
+++ test.sh
@@ -0,0 +1,14 @@
+git diff > test.patch
+
+if test ! -s "test.patch"
+then
+    echo is new version;
+else
+    rm -rf git_test
+    git clone  https://github.com/qortmdrjs159/git_test.git
+    qortmdrjs159
+    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 ;
+    echo is old version, update..
+fi
+
+rm -r test.patch
